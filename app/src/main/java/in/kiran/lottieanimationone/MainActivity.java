b2a8292da8a27ae27619213d39c134e6d2e726cc
kiranmahadik <felix@1234##>
2019-12-18 15:32:07 +0530

    Lottie animation demo using three different animations on single activity

---
+++ app/src/main/java/in/kiran/lottieanimationone/MainActivity.java
@@ -0,0 +1,21 @@
+package in.kiran.lottieanimationone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView mLottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLottieAnimationView = findViewById(R.id.lottieAnimationView1);
+
+    }
+}
